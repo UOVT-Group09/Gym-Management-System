fe1e40d97fda218ee57630c67c42790b2c1e6b30
Nishmi <sis24b115.uovt.ac.lk>
2026-05-02 15:35:08 +0530

    Fixed index.php content

---
+++ tier1_presentation/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gym Management System - Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Gym Management Dashboard</h1>
+        <div class="home-menu">
+            <a href="register.php" class="btn-submit">Add New Member</a>
+            <a href="view_members.php" class="btn-reset">View Members List</a>
+        </div>
+    </div>
+</body>
+</html>
